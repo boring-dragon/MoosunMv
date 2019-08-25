5773ad1d806effc75d0d549a67cdffeb33af88d0
Mohamed Jinas <j@live.mv>
2019-08-26 01:26:32 +0500

    renamed folder

--- test/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace Jinas\Moosun;
-require '../vendor/autoload.php';
-
-//"Gan", "kaadehdhoo", "Kahdhoo", "Hanimadhoo", "Male" are only available to pass
-$rp = new MoosunMv('Gan');
-echo $rp->temperature;
