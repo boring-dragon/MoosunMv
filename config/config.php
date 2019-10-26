68856496cc5f70978dea29576f97ec6cf9f55a9f
Mohamed Jinas <j@live.mv>
2019-10-27 02:42:07 +0500

    added a configuration file

---
+++ config/config.php
@@ -0,0 +1,7 @@
+<?php
+
+return array(
+    'Service' => 'Maldives Meteorology',
+    'Api_Url' => 'http://www.meteorology.gov.mv/fetchweather/',
+    'Scrap_Url' => 'http://www.meteorology.gov.mv/'
+);
