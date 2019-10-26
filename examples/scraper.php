d2e6f4d3a2a99375c6853132830af1f7f3ff7fde
Mohamed Jinas <j@live.mv>
2019-10-27 02:57:37 +0500

    examples added

---
+++ examples/scraper.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once '../vendor/autoload.php';
+
+use Jinas\Moosun\ImgScraper;
+
+$image = new ImgScraper;
+
+echo $image->satellite_image;
---
+++ examples/weather.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once '../vendor/autoload.php';
+
+use Jinas\Moosun\MoosunMv;
+
+$rp = new MoosunMv('Gan');
+echo $rp->stationname;
+echo $rp->temperature;
