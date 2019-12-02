a45bc735500f17a51224d56edc72efc099b70463
Mohamed Jinas <j@live.mv>
2019-12-03 00:30:01 +0500

    fixed config loading on laravel/lumen

--- src/Util.php
+++ src/Util.php
@@ -1,10 +1,16 @@
 <?php
+
 namespace Jinas\Moosun;
 
 class Util
 {
     public static function load($filepath)
     {
-        static::$items = include('../config/' . $filepath . '.php');
+        //Checking if the file exist
+        if (file_exists('../config/' . $filepath . '.php')) {
+            static::$items = include('../config/' . $filepath . '.php');
+        } else {
+            static::$items = include(base_path() . '/vendor/jinas/moosun/config/' . $filepath . '.php');
+        }
     }
 }
