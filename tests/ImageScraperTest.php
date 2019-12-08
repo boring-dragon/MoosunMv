79ae7f326c5d303e775c848f83e7e460f890a150
Mohamed Jinas <j@live.mv>
2019-12-08 05:34:04 +0500

    added imagescraper tests

---
+++ tests/ImageScraperTest.php
@@ -0,0 +1,27 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class ImageScraperTest extends TestCase
+{
+    public function testImageScraperShouldReturnAValidUrl()
+    {
+        $scraper = new \Jinas\Moosun\ImgScraper();
+        
+        $regex = "((https?|ftp)\:\/\/)?"; // SCHEME 
+        $regex .= "([a-z0-9+!*(),;?&=\$_.-]+(\:[a-z0-9+!*(),;?&=\$_.-]+)?@)?"; // User and Pass 
+        $regex .= "([a-z0-9-.]*)\.([a-z]{2,3})"; // Host or IP 
+        $regex .= "(\:[0-9]{2,5})?"; // Port 
+        $regex .= "(\/([a-z0-9+\$_-]\.?)+)*\/?"; // Path 
+        $regex .= "(\?[a-z+&\$_.-][a-z0-9;:@&%=+\/\$_.-]*)?"; // GET Query 
+        $regex .= "(#[a-z_.-][a-z0-9+\$_.-]*)?"; // Anchor 
+
+        $this->assertEquals(true, preg_match("/^$regex$/i", $scraper->satellite_image));
+    }
+
+    public function testImageScraperShouldReturnAValidJPGImage()
+    {
+        $scraper = new \Jinas\Moosun\ImgScraper();
+        $this->assertEquals(true, strpos($scraper->satellite_image, '.JPG'));
+    }
+}
